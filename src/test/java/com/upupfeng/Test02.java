e25484f8426abe5c0ac010561c137181f6b84770
upupfeng <69676213@qq.com>
2020-11-16 18:25:32 +0800

    delete some

--- src/test/java/com/upupfeng/Test02.java
+++
@@ -1,9 +0,0 @@
-package com.upupfeng;
-
-public class Test02 {
-    public static void main(String[] args) {
-        String a = "-";
-//        System.out.println(3 );
-        System.out.println("aaa" + null);
-    }
-}
